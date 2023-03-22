acd64c8e81e4e0aff6a844cff51e1b4378c3a6f5
Mathews <mathewsrodrigues9@gmail.com>
2023-03-22 16:21:02 -0300

    feat: :fire: Desenvolvendo função study_schedule
    Criado a função para percorer uma lista e identificar quantos alunos estão estudando o mesmo tempo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,2 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    pass
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    for period in permanence_period:
+        try:
+            if period[0] <= target_time <= period[1]:
+                counter += 1
+        except TypeError:
+            return None
+
+    return counter
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)], 3))

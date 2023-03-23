360f5cb761424cfdce397113928f5cb40cb74e47
Mathews <mathewsrodrigues9@gmail.com>
2023-03-23 13:58:02 -0300

    criando a funcao is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,17 @@
+# from functools import reduce
+
+
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    # reverse_word = word.lower()[::-1]
+
+    # reverse_word = reduce(lambda x, y: y + x, word)
+
+    reverse_word = ""
+    for i in word:
+        reverse_word = i + reverse_word
+    return reverse_word == word
+
+
+print(is_palindrome_iterative("agua"))
